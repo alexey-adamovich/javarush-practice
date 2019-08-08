a07b22dc90d26e6e518fdfec0b4bbd2bb8112bfc
alexey-adamovich <alex.adamovich83@gmail.com>
2019-08-08 20:52:05 +0300

    Java practice

---
+++ src/by/it/aadamovich/Task6/Solution.java
@@ -0,0 +1,29 @@
+package by.it.aadamovich.Task6;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] argh) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Character, Character> pairs = new HashMap<>();
+        pairs.put(')', '(');
+        pairs.put(']', '[');
+        pairs.put('}', '{');
+        while (sc.hasNext()) {
+            String input = sc.next();
+            //Complete the code
+
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < input.length(); i++) {
+                Character c = input.charAt(i);
+                if (!pairs.containsKey(c)) {
+                    stack.push(c);
+                } else if (stack.empty() || stack.pop() != pairs.get(c)) {
+                    stack.push(c);
+                    break;
+                }
+            }
+            System.out.println(stack.isEmpty());
+        }
+    }
+}
